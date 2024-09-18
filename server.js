a6864ac2bd797d40de8aa95446e50c072696843e
rajsrimann15 <rajsrimannwork@gmail.com>
2024-09-18 23:22:11 +0530

    first commit

--- server.js
+++ server.js
@@ -19,26 +19,4 @@ app.use('/', require("./routes/authRoutes"));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
-  
-  function sendRequestEvery8Minutes(url) {
-    // Helper function to send the request
-    async function sendRequest() {
-      try {
-        const response = await fetch(url);
-      } catch (error) {
-        console.error('Request failed:', error);
-      }
-    }
-  
-    // Initial request
-    sendRequest();
-  
-    // Create an interval to send requests every 8 minutes
-    setInterval(sendRequest, 5 * 60 * 1000); // Convert 8 minutes to milliseconds
-  }
-  
-  // Example usage:
-  const myUrl = 'https://shiftstream.onrender.com/admin/orders';
-  sendRequestEvery8Minutes(myUrl);
-  
 });
